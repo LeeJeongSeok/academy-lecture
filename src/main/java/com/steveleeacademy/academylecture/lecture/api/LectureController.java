7e5bccc0df4dff2893ab31436cb637868fa97d48
LeeJeongSeok <ljs0429777@gmail.com>
2022-01-22 14:55:05 +0900

    강의목록조회, 상세조회, 생성 메소드 추가

--- src/main/java/com/steveleeacademy/academylecture/lecture/api/LectureController.java
+++ src/main/java/com/steveleeacademy/academylecture/lecture/api/LectureController.java
@@ -1,12 +1,24 @@
 package com.steveleeacademy.academylecture.lecture.api;
 
+import com.steveleeacademy.academylecture.lecture.dto.LectureCreateDto;
+import com.sun.xml.bind.v2.TODO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.nio.charset.Charset;
+import java.util.*;
+
 @RestController
+@Slf4j
 public class LectureController {
 
     /**
@@ -14,21 +26,37 @@ public class LectureController {
      */
     @GetMapping("/")
     public ResponseEntity getLectures() {
-        return new ResponseEntity(HttpStatus.OK);
+//        HashMap<String, String> header = new HashMap<>();
+//        header.put("resultCode", "ok");
+//        header.put("resultMesg", "test");
+//
+//        List<String> headers = new ArrayList<>(header.keySet());
+
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        return new ResponseEntity("test", headers, HttpStatus.OK);
     }
 
     /**
      * 강의상세보기
      */
+    @GetMapping("/lectureDetail")
     public void lectureDetail() {
 
     }
 
     /**
      * 강의생성
+     * 강의 시퀀스 리턴?
      */
-    public void createLecture() {
+    // TODO : 강의 생성부터 우선적으로 실행되어야 함
+    @PostMapping("/createLecture")
+    @Valid
+    public ResponseEntity<String> createLecture(@Valid LectureCreateDto lectureCreateDto) {
+        log.info("createLecture info : {}", lectureCreateDto);
 
+        return ResponseEntity.ok().body("강의 생성 성공");
     }
 
     /**
