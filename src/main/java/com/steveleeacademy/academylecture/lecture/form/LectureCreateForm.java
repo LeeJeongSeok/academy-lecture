dc34006df2b6ac4cf83a537c87707fa770b311f7
LeeJeongSeok <ljs0429777@gmail.com>
2022-01-24 23:23:53 +0900

    강의 생성 코드 추가

--- src/main/java/com/steveleeacademy/academylecture/lecture/form/LectureCreateForm.java
+++ src/main/java/com/steveleeacademy/academylecture/lecture/form/LectureCreateForm.java
@@ -1,27 +1,42 @@
 package com.steveleeacademy.academylecture.lecture.form;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.steveleeacademy.academylecture.lecture.domain.*;
 import lombok.Getter;
+import lombok.ToString;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
+@ToString
 public class LectureCreateForm {
 
-    private List<Category> categories;
-    private List<Keyword> keywords;
+    private List<String> categories;
+    private List<String> keywords;
 
-    @NotNull
+    @NotEmpty(message = "강좌명은 필수 입력입니다.")
     private String title;
+
+    @NotEmpty(message = "강의내용은 필수 입력입니다.")
     private String content;
+
+    @NotEmpty(message = "강사명은 필수 입력입니다.")
     private String writer;
+
     private String startTime;
     private String endTime;
-    private List<Day> days;
-    private LocalDateTime stratDate;
-    private LocalDateTime endDate;
+    private List<String> days;
+
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDate startDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDate endDate;
+
     private String remainingLecture;
     private RegistStatus registStatus;
     private LectureStatus lectureStatus;
--- src/main/java/com/steveleeacademy/academylecture/lecture/service/LectureService.java
+++ src/main/java/com/steveleeacademy/academylecture/lecture/service/LectureService.java
@@ -1,7 +1,54 @@
 package com.steveleeacademy.academylecture.lecture.service;
 
+import com.steveleeacademy.academylecture.lecture.domain.Category;
+import com.steveleeacademy.academylecture.lecture.domain.Keyword;
+import com.steveleeacademy.academylecture.lecture.domain.Lecture;
+import com.steveleeacademy.academylecture.lecture.form.LectureCreateForm;
+import com.steveleeacademy.academylecture.lecture.repository.CategoryRepository;
+import com.steveleeacademy.academylecture.lecture.repository.DayRepository;
+import com.steveleeacademy.academylecture.lecture.repository.KeywordRepository;
+import com.steveleeacademy.academylecture.lecture.repository.LectureRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class LectureService {
+
+    private final LectureRepository lectureRepository;
+    private final CategoryRepository categoryRepository;
+    private final DayRepository dayRepository;
+    private final KeywordRepository keywordRepository;
+
+    public Long createLecture(LectureCreateForm lectureCreateForm) {
+
+        Lecture lecture = lectureRepository.save(Lecture.createLectureBuilder(lectureCreateForm));
+
+        List<String> categories = lectureCreateForm.getCategories();
+        List<String> keywords = lectureCreateForm.getKeywords();
+        List<String> days = lectureCreateForm.getDays();
+
+        lectureCreateForm.getCategories().forEach(category -> {
+            lecture.addCategory(new Category(category));
+        });
+
+        for (String category : categories) {
+            log.info("category : {}", category);
+        }
+
+        for (String keyword : keywords) {
+            log.info("keyword : {}", keyword);
+        }
+
+        for (String day : days) {
+            log.info("day : {}", day);
+        }
+
+        return lecture.getId();
+    }
+
 }
